cf80e25bb3ff489145e317821d1f15ff644006cc
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-05 14:10:18 +0200

    Add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "production",
+  entry: "./src/app.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
