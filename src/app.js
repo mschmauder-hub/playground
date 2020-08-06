2041a52fbe80c39935df3bd285fa8c89d865237d
mschmauder-hub <marius.schmauder.ms@gmail.com>
2020-08-06 10:54:43 +0200

    Add live-server

--- src/app.js
+++ src/app.js
@@ -5,5 +5,9 @@ import "./global.css";
 const toggleButton = createToggleButton();
 const title = createTitle();
 
+fetch("http://api.tvmaze.com/shows/1849")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
 document.body.append(title);
 document.body.append(toggleButton);
